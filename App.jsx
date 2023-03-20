c4169df33a4bfc1db4400453db6128fac0dab5dc
Orionn1412 <111780438+Orionn1412@users.noreply.github.com>
2023-03-20 21:54:37 +0700

    FE homepage

---
+++ App.jsx
@@ -0,0 +1,234 @@
+import { useEffect, useRef, useState } from "react";
+import React from "react";
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
+import Webcam from "react-webcam";
+import axios from "axios";
+import Logo from "./assets/transparent_white.png";
+import { BrowserRouter, NavLink, Route } from "react-router-dom";
+import UserCard from "./UserCard";
+import SignupForm from "./SignupForm";
+import LoginForm from "./LoginForm";
+import MainApp from "./MainApp";
+import FriendsApp from "./FriendsApp";
+//import SearchBar from "./SearchBar";
+
+export const BASE_URL = "http://localhost:8000";
+
+function App() {
+  // states
+  const [audioList, setAudioList] = useState([]); //mảng
+
+  const [apiAudioList, setApiAudioList] = useState([]); //api ban đầu
+  const [apiAudioListCopy, setApiAudioListCopy] = useState([]); //api ban đầu->copy
+
+  const [mood, setMood] = useState("");
+
+  const [audioInstance, setAudioInstance] = useState(null);
+
+  const [currAudio, setCurrAudio] = useState([]);
+
+  // refs
+  var webCamera = useRef(null);
+
+  async function fetchMusic() {
+    let response = await axios.post(`${BASE_URL}/api/post-image/`, {
+      image: webCamera.current.getScreenshot(),
+      token: localStorage.getItem("token"),
+    });
+    let songs = response.data.songs;
+    let resMood = response.data.mood;
+    setApiAudioList([]);
+    setApiAudioListCopy([]);
+    setApiAudioListCopy(
+      songs.map((song) => {
+        return {
+          name: song.name,
+          musicSrc: song.musicSrc,
+          cover: song.cover,
+        };
+      })
+    );
+    setApiAudioList(songs);
+    setMood(resMood);
+  }
+
+  useEffect(() => {
+    setAudioList(apiAudioList.slice(0, 6));
+  }, [apiAudioList, apiAudioListCopy]);
+
+  function playMusic(song) {
+    audioInstance.clear();
+    setTimeout(() => {
+      setCurrAudio([song]);
+      audioInstance.play();
+    }, 300);
+  }
+
+  function reverse(arr = [], l, r) {
+    while (l < r) {
+      var temp = arr[l];
+      arr[l] = arr[r];
+      arr[r] = temp;
+      l++;
+      r--;
+    }
+  }
+
+  function rotateDec(arr = []) {
+    reverse(arr, 0, arr.length - 2);
+    reverse(arr, 0, arr.length - 1);
+  }
+
+  function rotateInc(arr = []) {
+    reverse(arr, 1, arr.length - 1);
+    reverse(arr, 0, arr.length - 1);
+  }
+
+  function next() {
+    rotateInc(apiAudioList);
+    var em = [];
+    apiAudioList.slice(0, 6).map((song) => {
+      em.push(song);
+      return "";
+    });
+    setAudioList(em);
+  }
+
+  function prev() {
+    rotateDec(apiAudioList);
+    var em = [];
+    apiAudioList.slice(0, 6).map((song) => {
+      em.push(song);
+      return "";
+    });
+    setAudioList(em);
+  }
+
+  function logout() {
+    localStorage.clear();
+    window.location.pathname = "/login";
+  }
+
+  // function search() {
+  //   const [searchTerm, setSearchTerm] = useState("");
+  //   const [searchResults, setSearchResults] = useState([]);
+  //   const handleChange = (event) => {
+  //     setSearchTerm(event.target.value);
+  //   };
+  // }
+
+  return (
+    <div>
+      {/* top */}
+      <BrowserRouter>
+        <div className="d-flex h-90">
+          {/* menu */}
+          <div className="w-15 text-start pt-5" id="menu">
+            {/* logo */}
+            <div>
+              <img src={Logo} alt="logo" className="logo" />
+            </div>
+
+            {/* navs */}
+            <nav className="d-flex flex-column justify-content-around">
+              <NavLink to="/" activeClassName="active" exact>
+                <div>
+                  <div>
+                    <span>
+                      <i className="fas fa-home"></i>
+                    </span>{" "}
+                    Home
+                  </div>
+                </div>
+              </NavLink>
+              <NavLink to="/friends/" activeClassName="active" exact>
+                <div>
+                  <div>
+                    <span>
+                      <i className="fas fa-user-friends"></i>
+                    </span>{" "}
+                    Friends
+                  </div>
+                </div>
+              </NavLink>
+
+              <NavLink to="/Playlist" activeClassName="active" exact>
+                <div>
+                  <div>
+                    <span>
+                      <i className="fa fa-plus-square" aria-hidden="true"></i>
+                    </span>{" "}
+                    Playlist
+                  </div>
+                </div>
+              </NavLink>
+
+              {/* logout */}
+              <div onClick={() => logout()}>
+                <div>{localStorage.getItem("token") && "Logout"}</div>
+              </div>
+            </nav>
+
+            {/* logo */}
+            <div id="info">
+              <span>
+                <i className="far fa-copyright"></i>
+              </span>{" "}
+              Cap2-C2SE.23
+            </div>
+          </div>
+
+          <Route path="/" exact>
+            <MainApp
+              mood={mood}
+              webCamera={webCamera}
+              audioList={audioList}
+              apiAudioListCopy={apiAudioListCopy}
+              prev={prev}
+              next={next}
+              playMusic={playMusic}
+              Webcam={Webcam}
+              fetchMusic={fetchMusic}
+              //search = {search}
+            />
+          </Route>
+
+          <Route path="/friends" exact>
+            <FriendsApp UserCard={UserCard} />
+          </Route>
+
+          <Route path="/signup" exact>
+            <SignupForm />
+          </Route>
+
+          <Route path="/login" exact>
+            <LoginForm />
+          </Route>
+        </div>
+      </BrowserRouter>
+
+      {/* player */}
+      <div>
+        <ReactJkMusicPlayer
+          mode="full"
+          showDownload={false}
+          showDestroy={false}
+          showReload={false}
+          showLyric={false}
+          showThemeSwitch={false}
+          showPlayMode={false}
+          toggleMode={false}
+          audioLists={currAudio}
+          autoPlay={false}
+          seeked={false}
+          getAudioInstance={(audioObj) => {
+            setAudioInstance(audioObj);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
