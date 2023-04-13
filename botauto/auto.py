3c7cf3c7bb6e5a5af8b283636039c0a6bc31b0f7
minhlam97 <82572235+minhlam97@users.noreply.github.com>
2023-04-13 16:32:41 +0700

    ver 112, 2 cách file txt

--- botauto/auto.py
+++ botauto/auto.py
@@ -7,18 +7,18 @@ url = "https://dl3.parsiane.ir/music/a/adele/"
 driver = webdriver.Chrome()
 driver.get(url)
 
+
 mp3_links = driver.find_elements_by_xpath('//a[contains(@href, ".mp3")]')
 
-if not os.path.exists("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\adele"):
-    os.makedirs("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\adele")
+if not os.path.exists("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele"):
+    os.makedirs("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele")
 
 for link in mp3_links:
     url = link.get_attribute("href")
     response = requests.get(url)
     filename = url.split("/")[-1]
-    filepath = os.path.join("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\adele", filename)
+    filepath = os.path.join("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele", filename)
     with open(filepath, "wb") as f:
         f.write(response.content)
 
-# Đóng trình duyệt
 driver.quit()
