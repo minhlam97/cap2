76bb494c852322237ad6096962fae4ca4e63597b
Orionn1412 <111780438+Orionn1412@users.noreply.github.com>
2023-04-20 08:51:17 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeLayout from "../layouts/HomeLayout.vue";
+// import ProductLayout from "../layouts/ProductLayout.vue";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      redirect: "/",
+      component: HomeLayout,
+      children: [
+        {
+          path: "/",
+          component: () => import("../views/HomeView.vue"),
+        },
+        {
+          path: "/Search",
+          component: () => import("../views/SearchView.vue"),
+        },
+        {
+          path: "/Love-song",
+          component: () => import("../views/LikesongView.vue"),
+        },
+        {
+          path: "/Play-list",
+          component: () => import("../views/PlaylistView.vue"),
+        },
+        {
+          path: "/MyLibary",
+          component: () => import("../views/LibaryView.vue"),
+        },
+      ]
+    },
+  ],
+  scrollBehavior() {
+    window.scrollTo(0, 0);
+  },
+});
+
+export default router;
