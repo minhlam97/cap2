35e18409e995e626b640489bf0d37011b41cb9f4
minhlam97 <82572235+minhlam97@users.noreply.github.com>
2023-05-15 09:41:53 +0700

    update new

--- botauto/auto.py
+++ botauto/auto.py
@@ -2,22 +2,32 @@ from selenium import webdriver
 import selenium.webdriver as webdriver
 import requests
 import os
+from selenium.webdriver.common.by import By
 
-url = "https://dl3.parsiane.ir/music/a/adele/"
+
+# url = "https://dl3.parsiane.ir/music/a/adele/"
+# url = "https://dl2.beelody.com/Free/2021/7/Anne-Marie%20-%20Therapy%20%282021%29%20%5BBeelody%5D/" #freelink
+# url = "https://freepd.com/music/" #freelink100bai
+url = "http://sv2.mybia2music.com/s2/Music/1391/6%20Shahrivar/9201/Adele%20-%20Greatest%20Hits%20[128]/" 
+
+
+
 driver = webdriver.Chrome()
 driver.get(url)
 
+mp3_links = driver.find_elements(By.XPATH, '//a[contains(@href, ".mp3")]')
+# mp3_links = driver.find_elements_by_xpath('//a[contains(@href, ".mp3")]')
 
-mp3_links = driver.find_elements_by_xpath('//a[contains(@href, ".mp3")]')
 
-if not os.path.exists("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele"):
-    os.makedirs("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele")
+if not os.path.exists("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\adele"):
+    os.makedirs("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\adele")
 
 for link in mp3_links:
     url = link.get_attribute("href")
     response = requests.get(url)
     filename = url.split("/")[-1]
-    filepath = os.path.join("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\Songs\\Adele", filename)
+    song_title = filename.replace("%20" ," ")
+    filepath = os.path.join("C:\\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\adele", song_title)
     with open(filepath, "wb") as f:
         f.write(response.content)
 
--- botauto/up.py
+++ botauto/up.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-
+import time
+from selenium.webdriver.common.by import By
 
 url = 'http://127.0.0.1:8000/admin/api/song/add/'
-filename = 'path/to/your/file.mp3'
+# filename = 'path/to/your/file.mp3'
 
 driver = webdriver.Chrome()
 driver.get(url)
@@ -10,14 +11,27 @@ driver.get(url)
 username = "admin"
 password = "admin"
 
-username_input = driver.find_element_by_css_selector('input[name="username"]')
+username_input = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
 username_input.send_keys(username)
-password_input = driver.find_element_by_css_selector('input[name="password"]')
+password_input = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 password_input.send_keys(password)
-login_button = driver.find_element_by_css_selector('button[type="submit"]')
-login_button.click()
+login_button = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+time.sleep(5)
 
 
-mp3 = WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="id_mp3_file"]'))).click()
+name = driver.find_element(By.NAME, 'name').send_keys("admmin")
+    
+driver.find_element(By.ID, "id_poster").send_keys()
+    # ("C:\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\adele")
+    # poster = driver.find_element(By.XPATH, '//*[@id="id_poster"]').click()
+time.sleep(5)
+# poster_up = ("C:\Users\\OS\\OneDrive\\Máy tính\\CAP2\\lam\\adele").send_keys()
+    # poster=driver.find_element(By.XPATH, '//*[@id="id_poster"]').click()
+    # name1 = "Adele"
+    # name = driver.find_element(By.XPATH, '//*[@id="id_name"]').send_keys(name1)
+
+    # mp3_file=driver.find_element(By.XPATH, '//*[@id="id_mp3_file"]')
+
+    # mp3 = WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="id_mp3_file"]'))).click()
 
 
